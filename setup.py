abd2777f3ff80b4609c2fe6d61a89f6ce5ffd6e5
Anas Neumann <anas.neumann.isamm@gmail.com>
2023-11-11 17:03:06 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='MAML',
+    version='1.0',
+    description='A generic Python function that implements a simple version of the "Model-Agnostic Meta-Learning (MAML) Algorithm for Fast Adaptation of Deep Networks" as designed by Chelsea Finn et al. 2017',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Anas Neumann',
+    author_email='anas.neumann.1@ulaval.ca',
+    url='https://github.com/AnasNeumann/MAML',
+    py_modules=['maml'],
+    install_requires=required,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
+)
